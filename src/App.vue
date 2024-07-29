4a3174f9d648f490f07847591dbdf24626c846ad
naiba <hi@nai.ba>
2024-07-29 13:34:54 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/vue'
-const projectId = 'b011808e8bd2a2dfda13334db8afab70'
+import { BrowserProvider } from 'ethers';
+import { mainnet, bsc, avalanche, base, polygon, optimism, arbitrum, zkSync } from 'viem/chains'
+import { ref } from 'vue';
+
+const projectId = 'fe525a3fb7824f87c529d0935853cc2d'
 
 const metadata = {
   name: 'My Dapp',
@@ -11,18 +15,87 @@ const metadata = {
 
 const ethersConfig = defaultConfig({
   metadata,
+  auth: {
+    email: false
+  }
 })
+
+const toWCChainInfo = (chain: any) => {
+  return {
+    chainId: chain.id,
+    name: chain.name,
+    nativeCurrency: chain.nativeCurrency.name,
+    explorerUrl: chain.blockExplorers.default.url,
+    rpcUrl: chain.rpcUrls.default.http[0],
+  }
+}
+
+const chains = [toWCChainInfo(mainnet), toWCChainInfo(bsc), toWCChainInfo(avalanche),
+toWCChainInfo(base), toWCChainInfo(polygon), toWCChainInfo(optimism), toWCChainInfo(arbitrum), toWCChainInfo(zkSync)] as any
+
+console.log(chains);
 
 const modal = createWeb3Modal({
   ethersConfig,
   projectId,
-  enableAnalytics: true // Optional - defaults to your Cloud configuration  
+  chains,
+  enableAnalytics: false, // Optional - defaults to your Cloud configuration  
 })
+
+let ethersProvider: any
+let chainId = ref('--')
+
+const tx = ref({
+  to: '',
+  value: '',
+  data: '0x',
+  gasPrice: ''
+})
+
+const getSigner = async () => {
+  const walletProvider = modal.getWalletProvider()
+  ethersProvider = new BrowserProvider(walletProvider as any)
+  const network = await ethersProvider.getNetwork()
+  chainId.value = network.chainId
+  const signer = await ethersProvider.getSigner()
+  alert(signer?.address)
+}
+
+const sendTransaction = async () => {
+  try {
+    const txSend = {
+      "to": tx.value.to,
+      "value": tx.value.value,
+      "data": tx.value.data,
+      "gasPrice": tx.value.gasPrice
+    } as any
+    if (!txSend["gasPrice"]) {
+      delete txSend["gasPrice"]
+    }
+    console.log('sendTransaction', txSend);
+    const signer = await ethersProvider.getSigner()
+    const transaction = await signer.sendTransaction(txSend)
+    alert(transaction.hash)
+  } catch (error) {
+    console.error('sendTransaction', JSON.stringify(tx.value), error);
+  }
+}
+
 </script>
 
 <template>
   <main>
-    <button @click="modal.open()">Open Modal</button>
+    <button @click="modal.open()">Open Modal</button>&nbsp;
+    <button @click="modal.open({ view: 'Networks' })">Open Networks</button>&nbsp;
+    <button @click="getSigner()">Open Wallets</button>&nbsp;
+    <p>
+      ChainId: {{ chainId }} <br>
+      To: <input type="text" v-model="tx.to"> <br>
+      Value: <input type="text" v-model="tx.value"> <br>
+      Data: <input type="text" v-model="tx.data"> <br>
+      GasPrice: <input type="text" v-model="tx.gasPrice"> <br>
+      <button @click="sendTransaction()">Send Transaction</button>
+    </p>
   </main>
 </template>
 
